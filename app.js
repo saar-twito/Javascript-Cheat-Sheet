71e65a36359943a0f32a1a0e91affd770be52d66
Saar Twito <saartwito49@gmail.com>
2021-03-10 08:35:34 +0200

    Rename files and organize it better

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-
-
-for (let i = 0; i < 5; i++) {
-    // Count = 1 Bad (Not following the changes)
-    // Here the count don't have an accuses to the previous value.
-    // So we can say that it always be the value after the first rendering
-    setCount(count + 1);
-
-    // Count = 5 Good (Following the changes) ** Use This Way **
-    // Here the count has an accuses to the previous value.
-    // So for every rendering it will have the previous value and add to it. 
-    setCount((prevState) => prevState + 1);
-}
-
+// This file is for testing features
