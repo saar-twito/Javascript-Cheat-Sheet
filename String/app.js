2a62387cbb59a2ae26a9f3e7303ba12e87a72456
saartwito <saartwito49@gmail.com>
2022-02-21 05:32:42 +0200

    .

--- String/app.js
+++ String/app.js
@@ -13,10 +13,10 @@ console.log(sentence2.substring(2)); // llo world!
 
 
 
+// Note: Only working on array.
 // Note: This method changes the original array.
-// array.splice(index, howMany, item1, ....., itemX)
+// array.splice(index, howManyToRemove, item1, ....., itemX)
 // Index - An integer that specifies at what position to add/remove/get items {R}
-// howMany - The number of items to be removed {O}
 const sign = ["A", "B", "C", "D"];
 sign.splice(2, 0, "1", "2"); // [ 'A', 'B', '1', '2', 'C', 'D' ]
 sign.splice(2, 2) // [ 'A', 'B' ]
@@ -24,8 +24,10 @@ sign.splice(2); // [ 'A', 'B' ]
 
 
 
-// Slice - return the selected element in new array
-// array.slice(start, end - 1)
-const numbers = [1, 2, 3, 4, 5]
-console.log(numbers.slice(2)) // [ 3, 4, 5 ]   
-console.log(numbers.slice(0, 2)) // [ 1, 2 ]
+// Slice - return the selected elements
+// .slice(start, end - 1)
+const numbers = [1, 2, 3, 4, 5] 
+const numbersAsString = '123456' 
+
+console.log(numbers.slice(2)) // [ 3, 4, 5 ] || "3456" 
+console.log(numbers.slice(0, 2)) // [ 1, 2 ] || "12" 
