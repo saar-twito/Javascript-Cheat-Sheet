8fbde65f6a16ff90d4554c250899403a4b51b654
saartwito <saartwito49@gmail.com>
2022-02-21 05:10:44 +0200

    .

--- String/app.js
+++ String/app.js
@@ -1,10 +1,8 @@
-// Split - used to split a string.array and returns the new array. 
+// Split - used to split a string only and returns the new array. 
 // string.split(separator, limit) 
 const sentence1 = "How are you";
-console.log(`1 ${sentence1.split()}`); // copy - How are you
-console.log(`2 ${sentence1.split("")}`); // H,o,w, ,a,r,e, ,y,o,u
-console.log(`3 ${sentence1.split(" ", 3)}`); // How,are,you
-console.log(`4 ${sentence1.split("o")}`); // H,w are y,u
+console.log(`${sentence1.split(" ")}`); // [How, are, you]
+console.log(`${sentence1.split("o")}`); // [H,w, are, y,u]
 
 
 // Substring - extracting the characters from a string by index and return the those characters as a string.
@@ -12,7 +10,6 @@ console.log(`4 ${sentence1.split("o")}`); // H,w are y,u
 const sentence2 = "Hello world!";
 console.log(sentence2.substring(1, 4)); // ell
 console.log(sentence2.substring(2)); // llo world!  
-console.log(sentence2.substring()); // copy - Hello world!
 
 
 
@@ -27,9 +24,8 @@ sign.splice(2); // [ 'A', 'B' ]
 
 
 
-// Slice -  return the selected element in new array
+// Slice - return the selected element in new array
 // array.slice(start, end - 1)
 const numbers = [1, 2, 3, 4, 5]
-console.log(numbers.slice()) // copy - [1, 2, 3, 4, 5]
 console.log(numbers.slice(2)) // [ 3, 4, 5 ]   
 console.log(numbers.slice(0, 2)) // [ 1, 2 ]
