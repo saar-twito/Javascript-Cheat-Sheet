dd9fefecd2089da527c17fd0c0fe76a39e173c02
saartwito <saartwito49@gmail.com>
2022-03-17 11:03:52 +0200

    .

--- String/app.js
+++ String/app.js
@@ -14,9 +14,9 @@ console.log(sentence2.substring(2)); // llo
 
 
 
-// slice() returns selected elements in an array, as a new array.
+// slice() returns selected elements.
 // .slice(start, end - 1)
 const numbers = [1, 2, 3, 4, 5];
 const numbersAsString = '123456';
 console.log(numbers.slice(2)) // [ 3, 4, 5 ] 
-console.log(numbersAsString.slice(0, 2)) // [ 1, 2 ]
+console.log(numbersAsString.slice(0, 2)) // 12
